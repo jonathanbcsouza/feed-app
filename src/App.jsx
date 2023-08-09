fddc42c026a65426119fa65a7ddfaaafc97c814a
jonathanbcsouza <jonathanbcsouza@gmail.com>
2023-08-09 22:38:22 +1200

    Add sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Post } from './Post'
 import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
+
+import styles from './App.module.css'
 
 import './global.css'
 
@@ -7,16 +10,19 @@ export function App() {
   return (
     <>
       <Header />
-
-      <Post
-        author="John Doe"
-        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque, tenetur."
-      />
-
-      <Post
-        author="James Smith"
-        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur, delectus."
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="John Doe"
+            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque, tenetur."
+          />
+          <Post
+            author="James Smith"
+            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur, delectus."
+          />
+        </main>
+      </div>
     </>
   )
 }
