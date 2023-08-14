4655cf0f8af35d45df49e57d90d993fa3b1079d2
jonathanbcsouza <jonathanbcsouza@gmail.com>
2023-08-14 22:43:22 +1200

    Add styling

---
+++ src/components/Comment.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import styles from './Comment.module.css';
+
+import { ThumbsUp, Trash } from 'phosphor-react';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/jonathanbcsouza.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Diego Fernandes</strong>
+              <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">1 hour ago</time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Well done Devon, congrats!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
