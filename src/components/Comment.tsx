f66d5544dd13184be717b44fe9cbd157c2d7764a
jonathanbcsouza <jonathanbcsouza@gmail.com>
2023-08-19 11:25:41 +1200

    Edit descriptions

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import styles from './Comment.module.css';
+
+import styles from './Comment.module.css'
 
 import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
@@ -7,17 +8,17 @@ import { Avatar } from './Avatar';
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <Avatar hasBorder={false} src="https://github.com/jonathanbcsouza.png" alt="" />
+      <Avatar hasBorder={false} src="https://github.com/jonathanbcsouza.png"/>
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
           <header>
             <div className={styles.authorAndTime}>
-              <strong>Diego Fernandes</strong>
+              <strong>Jonathan Souza</strong>
               <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">1 hour ago</time>
             </div>
 
-            <button title="Deletar comentário">
+            <button title="Delete comment">
               <Trash size={24} />
             </button>
           </header>
@@ -28,7 +29,7 @@ export function Comment() {
         <footer>
           <button>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Likes <span>20</span>
           </button>
         </footer>
       </div>
